f71d6342d146c99b957ecd76ced4e03000b3ce07
ahmeddevpract-cloud <ahmeddevpract@gmail.com>
2026-04-08 06:20:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,14 @@ function scrollToSection(id){
   document.getElementById(id).scrollIntoView({behavior:'smooth'});
 }
 
+const toggle = document.getElementById('menu-toggle');
+const nav = document.getElementById('nav-links');
+
+if(toggle){
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+}
+
 ScrollReveal().reveal('.hero h2',{delay:200});
 ScrollReveal().reveal('.card',{interval:150});
